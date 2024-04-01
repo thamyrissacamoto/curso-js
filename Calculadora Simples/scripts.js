0de3c7979205ea3a9ea61d0af66d1a2b2af783c8
Thamyris Sacamoto <thamyrissacamoto@hotmail.com>
2024-04-01 16:32:06 -0300

    calculadora simples

---
+++ Calculadora Simples/scripts.js
@@ -0,0 +1,84 @@
+// Seleção dos elementos
+const display = document.querySelector("#displayInput");
+const botaoIgual = document.querySelector(".igual");
+const botaoPonto = document.querySelector(".ponto");
+const botoesNumeros = document.querySelectorAll(".num");
+const botoesOperadores = document.querySelectorAll(".operador");
+
+// Variáveis globais
+let operacaoAtual = "";
+let operador = null;
+let valorAnterior = "";
+let calculando = false;
+
+// Funções
+function atualizaDisplay() {
+  console.log();
+  display.value = operacaoAtual;    
+}
+
+function insereNumero(evento) {
+  if (calculando) {
+    operacaoAtual = evento.target.textContent;
+    calculando = false;
+  } else {
+    operacaoAtual += evento.target.textContent;
+  }
+  atualizaDisplay();
+}
+
+function insereOperador(evento) {
+  if (operacaoAtual !== "") {
+    if (!calculando) {
+      if (operador !== null) calcula();
+      valorAnterior = operacaoAtual;
+      operacaoAtual = "";
+    }
+    operador = evento.target.textContent;
+  }
+}
+
+function calcula() {
+  let resultado = null;
+  const operandoAnterior = parseFloat(valorAnterior);
+  const operandoAtual = parseFloat(operacaoAtual);
+
+  switch (operador) {
+    case "+":
+      resultado = operandoAnterior + operandoAtual;
+      break;
+    case "-":
+      resultado = operandoAnterior - operandoAtual;
+      break;
+    case "*":
+      resultado = operandoAnterior * operandoAtual;
+      break;
+    case "/":
+      if (operandoAtual !== 0) {
+        resultado = operandoAnterior / operandoAtual;
+      } else {
+        alert("Erro: Divisão por zero não é permitida!");
+        return;
+      }
+      break;
+  }
+  operacaoAtual = String(resultado);
+  valorAnterior = operacaoAtual;
+  calculando = true;
+  atualizaDisplay();
+}
+
+function inserePonto() {
+  if (operacaoAtual.indexOf(".") === -1) {
+    operacaoAtual += ".";
+    atualizaDisplay();
+  }
+}
+
+// EventListeners
+botaoPonto.addEventListener("click", inserePonto);
+botoesNumeros.forEach((botao) => botao.addEventListener("click", insereNumero));
+botoesOperadores.forEach((botao) =>
+  botao.addEventListener("click", insereOperador)
+);
+botaoIgual.addEventListener("click", calcula);
